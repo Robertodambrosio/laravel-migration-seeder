6f38a6ee3f66a49b585b5ad5faad6eb72ebf7ad4
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-07 15:35:26 +0100

    first commit

---
+++ database/migrations/2022_02_07_142754_create_packets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePacketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packets', function (Blueprint $table) {
+            $table->id();
+            $table->string("departure");
+            $table->string("destination");
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->tinyInteger('people_nr');
+            $table->float('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packets');
+    }
+}
